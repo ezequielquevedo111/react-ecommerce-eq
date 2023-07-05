e7528ddbd03ffa1b383e2edd097669f06ee7c6ff
Ezequiel Quevedo <ezequielquevedo111@gmail.com>
2023-07-04 21:56:38 -0300

    cambio en el cart widget para que sea link

--- src/components/common/CartWidget/cartWidget/CartWidget.jsx
+++ src/components/common/CartWidget/cartWidget/CartWidget.jsx
@@ -1,12 +1,20 @@
 import { Badge, Box } from "@chakra-ui/layout";
+import { BreadcrumbLink } from "@chakra-ui/react";
 import { GiCauldron } from "react-icons/gi";
 const CartWidget = () => {
   return (
-    <Box display={"flex"} flexDirection={"row-reverse"} paddingLeft={"10px"}>
-      <Badge color={"#c68e01"} backgroundColor={"#0e1428"}>
-        {"3"}
-      </Badge>
-      <GiCauldron color="#c68e01" size={"30px"} />
+    <Box
+      display={"flex"}
+      flexDirection={"row-reverse"}
+      paddingLeft={"10px"}
+      href={"#"}
+    >
+      <BreadcrumbLink display={"flex"} flexDirection={"row-reverse"}>
+        <Badge color={"#c68e01"} backgroundColor={"#0e1428"}>
+          {"3"}
+        </Badge>
+        <GiCauldron color="#c68e01" size={"30px"} />
+      </BreadcrumbLink>
     </Box>
   );
 };
