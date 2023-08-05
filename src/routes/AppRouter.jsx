d2940ccf1f22b357f6d8506487aa1df2ec03810d
Ezequiel Quevedo <ezequielquevedo111@gmail.com>
2023-08-05 18:26:38 -0300

    ultimo fix?

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import SaveErrorPage from "./SaveErrorPage";
-import { simpleRoutes } from "./SimpleRoutes/simpleRoutes";
+import { simpleRoutes } from "../../src/routes/SimpleRoutes/simpleRoutes";
 
 const AppRouter = () => {
   return (
